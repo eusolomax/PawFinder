dbeb0b4fee2773af164a13b90ad631f29ec56f27
eusolomax <capitellivictor@gmail.com>
2024-04-27 17:42:15 -0300

    feature: Sequelize and minors packages; feature: Using Typescript;

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,9 @@
+require('dotenv').config()
+
 const express = require('express')
+const db = require('./db');
 const app = express()
-const port = 8080;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
